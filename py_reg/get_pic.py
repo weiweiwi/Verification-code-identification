5a66a758ca24d26a4ab1fd7e69f9d60717b3686e
1092758202 <1092758202@qq.com>
2018-12-26 19:29:45 +0800

    replace some picture

--- py_reg/get_pic.py
+++ py_reg/get_pic.py
@@ -2,22 +2,20 @@
 import requests
 import os
 
-code_url = 'https://cd.abiz.com/validcode'  # 验证码URL
-
+code_url = 'http://202.193.80.58:81/academic/getCaptcha.do?0.23611265529353553'  # 验证码URL
 
 
 def getImg(val,localPath):
     if(not os.path.exists(localPath)):
         os.mkdir(localPath)
-
     for i in range(val,10000) :
-        ir=requests.get(code_url+'?t='+str(i))
+        ir=requests.get(code_url)
         str1=localPath+'%d.jpg'%i
         print(str1)
+        print(ir.content)
         open(str1 ,'wb').write(ir.content)
 
 if __name__=='__main__':
-
-    getImg(1000,'e:/pic/')
+   getImg(1,'C:/Users/dell/Desktop/Verification-code-identification/pic/')
 
 
